53672579457e338e2bbb90dae13f5de31b295508
Manohar Reddy <201451024@iiitvadodara.ac.in>
2018-04-24 14:06:05 +0530

    temporary commit

--- libvmi/driver/bareflank/bareflank.c
+++ libvmi/driver/bareflank/bareflank.c
@@ -106,3 +106,75 @@ bareflank_destroy(
 
     vmi->driver.driver_data = NULL;
 }
+
+static status_t
+bareflank_get_vcpuregs_hvm(
+    vmi_instance_t vmi,
+    registers_t *regs,
+    unsigned long vcpu)
+{
+    xen_instance_t *bareflank = bareflank_get_instance(vmi);
+    struct hvm_hw_cpu hw_ctxt = {0}, *hvm_cpu = NULL;
+
+    if (NULL == hvm_cpu) {
+        if (xen->libxcw.xc_domain_hvm_getcontext_partial(xen->xchandle,
+                xen->domainid,
+                HVM_SAVE_CODE(CPU),
+                vcpu,
+                &hw_ctxt,
+                sizeof hw_ctxt)) {
+            errprint("Failed to get context information (HVM domain).\n");
+            return VMI_FAILURE;
+        }
+        hvm_cpu = &hw_ctxt;
+    }
+
+    regs->x86.rax = hvm_cpu->rax;
+    regs->x86.rbx = hvm_cpu->rbx;
+    regs->x86.rcx = hvm_cpu->rcx;
+    regs->x86.rdx = hvm_cpu->rdx;
+    regs->x86.rbp = hvm_cpu->rbp;
+    regs->x86.rsi = hvm_cpu->rsi;
+    regs->x86.rdi = hvm_cpu->rdi;
+    regs->x86.rsp = hvm_cpu->rsp;
+    regs->x86.r8 = hvm_cpu->r8;
+    regs->x86.r9 = hvm_cpu->r9;
+    regs->x86.r10 = hvm_cpu->r10;
+    regs->x86.r11 = hvm_cpu->r11;
+    regs->x86.r12 = hvm_cpu->r12;
+    regs->x86.r13 = hvm_cpu->r13;
+    regs->x86.r14 = hvm_cpu->r14;
+    regs->x86.r15 = hvm_cpu->r15;
+    regs->x86.rip = hvm_cpu->rip;
+    regs->x86.rflags = hvm_cpu->rflags;
+    regs->x86.cr0 = hvm_cpu->cr0;
+    regs->x86.cr2 = hvm_cpu->cr2;
+    regs->x86.cr3 = hvm_cpu->cr3;
+    regs->x86.cr4 = hvm_cpu->cr4;
+    regs->x86.dr7 = hvm_cpu->dr7;
+    regs->x86.fs_base = hvm_cpu->fs_base;
+    regs->x86.gs_base = hvm_cpu->gs_base;
+    regs->x86.cs_arbytes = hvm_cpu->cs_arbytes;
+    regs->x86.sysenter_cs = hvm_cpu->sysenter_cs;
+    regs->x86.sysenter_esp = hvm_cpu->sysenter_esp;
+    regs->x86.sysenter_eip = hvm_cpu->sysenter_eip;
+    regs->x86.msr_efer = hvm_cpu->msr_efer;
+    regs->x86.msr_star = hvm_cpu->msr_star;
+    regs->x86.msr_lstar = hvm_cpu->msr_lstar;
+
+    return VMI_SUCCESS;
+}
+
+status_t
+xen_get_vcpuregs(
+    vmi_instance_t vmi,
+    registers_t *regs,
+    unsigned long vcpu)
+{
+#if defined(I386) || defined (X86_64)
+    if (vmi->vm_type == HVM)
+        return xen_get_vcpuregs_hvm(vmi, regs, vcpu);
+#endif
+
+    return VMI_FAILURE;
+}
